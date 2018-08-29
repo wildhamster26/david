6dd7e079617a7c21c51eb23612930da20cceeacb
wildhamster26 <38760849+wildhamster26@users.noreply.github.com>
2018-08-29 21:22:34 +0200

    Added script, art and film sections with buttons and hidden contents

--- david.js
+++ david.js
@@ -1,15 +1,22 @@
 let scriptsButton = document.querySelector("#scriptsButton")
 let scripts = document.querySelector("#scripts");
 
-
 scriptsButton.addEventListener("click", function(){
 	scripts.classList.toggle("visibility");
 });
 
+
 let artButton = document.querySelector("#artButton")
 let art = document.querySelector("#art");
 
-
 artButton.addEventListener("click", function(){
 	art.classList.toggle("visibility");
+});
+
+
+let filmButton = document.querySelector("#filmButton")
+let film = document.querySelector("#film");
+
+filmButton.addEventListener("click", function(){
+	film.classList.toggle("visibility");
 });
